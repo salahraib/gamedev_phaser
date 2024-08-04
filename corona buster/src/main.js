1aa1595ce5c8f31f3e8ceec6aa9ecb16c1ae4476
salahraib <sm9423299@gmail.com>
2024-08-04 13:47:00 +0800

    import game over scene

--- corona buster/src/main.js
+++ corona buster/src/main.js
@@ -1,29 +1,23 @@
 import Phaser from 'phaser'
 import CoronaBusterScene from './scenes/CoronaBusterScene'
+import GameOverScene from './scenes/GameOverScene'
 
-
-
-	
-
-		const config = {
-			type: Phaser.AUTO,
-			parent:"app",
-			width: 400,
-			height: 620,
-			physics: {
-			default: 'arcade',
-			arcade: {
-			
+const config = {
+	type: Phaser.AUTO,
+	parent: "app",
+	width: 400,
+	height: 620,
+	physics: {
+		default: 'arcade',
+		arcade: {
 			gravity: { y: 0 }
-			}
-			},
-			scene: [CoronaBusterScene],
-			scale : {
-			mode :Phaser.Scale.FIT,
-			autoCenter : Phaser.Scale.CENTER_BOTH
-			},
-			}
-
+		}
+	},
+	scene: [CoronaBusterScene, GameOverScene],
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	},
+}
 
 export default new Phaser.Game(config)
-
